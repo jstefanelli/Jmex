53e2a2d3c17d83c603ba4fc7a0c49b79a403e7ee
JPSgfx <jpsdzn@gmail.com>
2015-06-12 17:01:56 +0200

    Main Header file. Connection to the server and selection of the database

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password  = "";
+
+	$conn = mysqli_connect($servername, $username, $password);
+
+
+	if(!$conn){
+		die("Connection failed: " . mysqli_connect_error());
+	}
+    $db = mysqli_select_db($conn, "jmex");
+    if(!$db){
+        die("database not found");
+    }
+    mb_internal_encoding('UTF-8');
+?>
