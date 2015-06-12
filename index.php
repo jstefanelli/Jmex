193f109b36d780222941ecccf6138336f71b8b54
JPSgfx <jpsdzn@gmail.com>
2015-06-12 14:05:19 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>JMex beta</title>
+        <script type="text/javascript" src="scripts.js"></script>
+    </head>
+    <body>
+        <div>
+            <p id="txt">Hey! Title</p>
+            <button type="button" onClick = "changeName()">Test</button>
+        </div>
+    </body>
+</html>
---
+++ scripts.js
@@ -0,0 +1,6 @@
+function changeName() {
+    var text = document.getElementById('txt');
+    document.title = 'Test!';
+    text.innerHTML = 'Title changed! WOOOOOO!';
+    window.alert('Test!');
+}
